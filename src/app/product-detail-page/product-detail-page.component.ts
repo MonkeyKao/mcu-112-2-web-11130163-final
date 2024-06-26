4b12a0a30ca568c27f8a9615849e325433e26120
MonkeyKao <sunnylovemonkey@gmail.com>
2024-06-26 19:40:34 +0800

    feat: 產品檢視頁面新增

---
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -0,0 +1,27 @@
+import { CurrencyPipe, JsonPipe } from '@angular/common';
+import { Component, Input, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from '../model/product';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-product-detail-page',
+  standalone: true,
+  imports: [CurrencyPipe, JsonPipe],
+  templateUrl: './product-detail-page.component.html',
+  styleUrl: './product-detail-page.component.css',
+})
+export class ProductDetailPageComponent {
+  @Input()
+  product!: Product;
+
+  private router = inject(Router);
+
+  private productService = inject(ProductService);
+
+  onAddToCart(): void {}
+
+  onBack(): void {
+    this.router.navigate(['products']);
+  }
+}
