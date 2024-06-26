c345adcbda67c0b03d7ffe3e77212a4bd26dae30
MonkeyKao <sunnylovemonkey@gmail.com>
2024-06-26 21:44:30 +0800

    feat: 購物車表單定義

---
+++ src/app/interface/order-form.interface.ts
@@ -0,0 +1,12 @@
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { IOrderDetailForm } from './order-detail-form.interface';
+
+export interface IOrderForm {
+  name: FormControl<string | null>;
+
+  address: FormControl<string | null>;
+
+  telephone: FormControl<string | null>;
+
+  details: FormArray<FormGroup<IOrderDetailForm>>;
+}
--- src/app/shopping-cart-page/shopping-cart-page.component.ts
+++ src/app/shopping-cart-page/shopping-cart-page.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { IOrderDetailForm } from '../interface/order-detail-form.interface';
+import { IOrderForm } from '../interface/order-form.interface';
 
 @Component({
   selector: 'app-product-detail-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './shopping-cart-page.component.html',
   styleUrl: './shopping-cart-page.component.css',
 })
-export class ShoppingCartPageComponent {}
+export class ShoppingCartPageComponent {
+  readonly form = new FormGroup<IOrderForm>({
+    name: new FormControl<string | null>(null),
+
+    address: new FormControl<string | null>(null),
+
+    telephone: new FormControl<string | null>(null),
+
+    details: new FormArray<FormGroup<IOrderDetailForm>>([]),
+  });
+
+  get name(): FormControl<string | null> {
+    return this.form.get('name') as FormControl<string | null>;
+  }
+
+  get address(): FormControl<string | null> {
+    return this.form.get('address') as FormControl<string | null>;
+  }
+
+  get telephone(): FormControl<string | null> {
+    return this.form.get('telephone') as FormControl<string | null>;
+  }
+
+  get details(): FormArray<FormGroup<IOrderDetailForm>> {
+    return this.form.get('details') as FormArray<FormGroup<IOrderDetailForm>>;
+  }
+}
